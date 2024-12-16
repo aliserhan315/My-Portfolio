d101c36bd13919b21e9ee9890d16558487c3a959
aliserhan315 <aliserhan315@gmail.com>
2024-12-16 23:09:00 +0200

    building

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output:'export',
+  typescript:{
+    ignoreBuildErrors:true
+  }
+ 
 };
 
 export default nextConfig;
